f7809464da3b3fa41b6d62ca2bf094f347ade917
Lilia Lomaka <llomaka80@gmail.com>
2022-08-10 14:44:06 +0300

    Adding contact and request processing functions

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,57 @@
-const express = require('express')
+  const express = require('express')
 
 const router = express.Router()
 
+const { listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact } = require('../../models/contacts')
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(await listContacts())
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const contacts = await listContacts()
+  if (!contacts.find(contact => contact.id === contactId)) {
+    res.status(404).send('Check data before next request')
+  } else {
+    res.json(await getContactById(contactId))
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { name, email, phone } = req.body
+  if (!name || !email || !phone) {
+    res.status(400).send('Check data before next request')
+  } else {
+    res.status(201).json(await addContact(req.body))
+  }  
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const contacts = await listContacts()
+  if (!contacts.find(contact => contact.id === contactId)) {
+    res.status(404).send('Check data before next request')
+  } else {
+    res.json(await removeContact(contactId))
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const { name, email, phone } = req.body
+  const contacts = await listContacts()
+  if (!contacts.find(contact => contact.id === contactId)) {
+    res.status(404).send('Check data before next request')
+  } else if (!name || !email || !phone) {
+    res.status(400).send('Check data before next request')
+  } else {
+    res.json(await updateContact(contactId, req.body))
+  }  
 })
 
 module.exports = router
