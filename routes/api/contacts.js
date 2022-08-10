a52d9917a94a3e826e5b2de746943240005bbc44
Lilia Lomaka <llomaka80@gmail.com>
2022-08-10 16:07:08 +0300

    Adding joi validation for post and put methods

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,33 @@
-  const express = require('express')
+const express = require('express')
 
 const router = express.Router()
+
+const Joi = require('joi');
+
+const schemaAddContact = Joi.object({
+  name: Joi.string()
+    .pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)
+    .required(),
+
+  email: Joi.string()
+    .email({ minDomainSegments: 2})
+    .required(),
+    
+  phone: Joi.string()
+    .pattern(/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/)
+    .required(),
+})
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string()
+    .pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/),
+
+  email: Joi.string()
+    .email({ minDomainSegments: 2}),
+    
+  phone: Joi.string()
+    .pattern(/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/),
+})
 
 const { listContacts,
   getContactById,
@@ -16,7 +43,7 @@ router.get('/:contactId', async (req, res, next) => {
   const { contactId } = req.params
   const contacts = await listContacts()
   if (!contacts.find(contact => contact.id === contactId)) {
-    res.status(404).json({"message": "Not found"})
+    res.status(404).json({ "message": "Not found" })
   } else {
     res.json(await getContactById(contactId))
   }
@@ -24,8 +51,9 @@ router.get('/:contactId', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   const { name, email, phone } = req.body
-  if (!name || !email || !phone) {
-    res.status(400).json({"message": "missing required name field"})
+  const result = schemaAddContact.validate({ name: name, email: email, phone: phone })
+  if (!name || !email || !phone || result.error) {
+    res.status(400).json({ "message": "missing required name field" })
   } else {
     res.status(201).json(await addContact(req.body))
   }  
@@ -44,10 +72,11 @@ router.delete('/:contactId', async (req, res, next) => {
 
 router.put('/:contactId', async (req, res, next) => {
   const { contactId } = req.params
+  const result = schemaUpdateContact.validate({ name: req.body.name, email: req.body.email, phone: req.body.phone })
   const contacts = await listContacts()
   if (!contacts.find(contact => contact.id === contactId)) {
     res.status(404).json({"message": "Not found"})
-  } else if (!req.body) {
+  } else if (!req.body || result.error) {
     res.status(400).json({"message": "missing fields"})
   } else {
     res.json(await updateContact(contactId, req.body))
