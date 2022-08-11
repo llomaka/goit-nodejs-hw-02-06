b84e561060ee7d939ae9dd2b7e0465a72fd60e27
Lilia Lomaka <llomaka80@gmail.com>
2022-08-11 08:34:43 +0300

    Adding trycatch to API

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -11,49 +11,70 @@ const { listContacts,
   updateContact } = require('../../models/contacts')
 
 router.get('/', async (req, res, next) => {
-  res.json(await listContacts())
+  try {
+    res.json(await listContacts())  
+  } catch (error) {
+    console.log(error.message)
+  }
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params
-  const contacts = await listContacts()
-  if (!contacts.find(contact => contact.id === contactId)) {
-    res.status(404).json({ "message": "Not found" })
-  } else {
-    res.json(await getContactById(contactId))
+  try {
+    const { contactId } = req.params
+    const contacts = await listContacts()
+    if (!contacts.find(contact => contact.id === contactId)) {
+      res.status(404).json({ "message": "Not found" })
+    } else {
+      res.json(await getContactById(contactId))
+    }
+  } catch (error) {
+    console.log(error.message)
   }
 })
 
 router.post('/', async (req, res, next) => {
-  const { value, error } = schemaAddContact.validate(req.body)
-  if (error) {
-    res.status(422).json({ "message": error.message })
-  } else {
-    res.status(201).json(await addContact(value))
+  try {
+    const { value, error } = schemaAddContact.validate(req.body)
+    if (error) {
+      res.status(422).json({ "message": error.message })
+    } else {
+      res.status(201).json(await addContact(value))
+    }
+  } catch (error) {
+    console.log(error.message)
   }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params
-  const contacts = await listContacts()
-  if (!contacts.find(contact => contact.id === contactId)) {
-    res.status(404).json({ "message": "Not found" })
-  } else {
-    await removeContact(contactId)
-    res.json({ "message": "contact deleted" })
+  try {
+    const { contactId } = req.params
+    const contacts = await listContacts()
+    if (!contacts.find(contact => contact.id === contactId)) {
+      res.status(404).json({ "message": "Not found" })
+    } else {
+      await removeContact(contactId)
+      res.json({ "message": "contact deleted" })
+    }
+  } catch (error) {
+    console.log(error.message)
   }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params
-  const { value, error } = schemaUpdateContact.validate(req.body)
-  const contacts = await listContacts()
-  if (!contacts.find(contact => contact.id === contactId)) {
-    res.status(404).json({ "message": "Not found" })
-  } else if (Object.keys(req.body).length === 0 || error) {
-    res.status(400).json({ "message": "missing fields" })
-  } else {
-    res.json(await updateContact(contactId, value))
+  try {
+    const { contactId } = req.params
+    const { value, error } = schemaUpdateContact.validate(req.body)
+    const contacts = await listContacts()
+    if (!contacts.find(contact => contact.id === contactId)) {
+      res.status(404).json({ "message": "Not found" })
+    } else if (Object.keys(req.body).length === 0 || error) {
+      res.status(400).json({ "message": "missing fields" })
+    } else {
+      res.json(await updateContact(contactId, value))
+    }
+  } catch (error) {
+    console.log(error.message)
   }
 })
 
