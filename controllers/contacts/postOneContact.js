1e8300f4fa9a407c8c8169d79bb3d0ecda9c1921
Lilia Lomaka <llomaka80@gmail.com>
2022-08-17 19:24:40 +0300

    Checking request body before adding contact

--- controllers/contacts/postOneContact.js
+++ controllers/contacts/postOneContact.js
@@ -1,10 +1,12 @@
 const { addContactData } = require('../../services/contacts')
+const {RequestError} = require('../../helpers')
 
 const postOneContact = async (req, res) => {
-    res.status(201).json(await addContactData(req.body))
-    //    if (error) {
-    //         res.status(422).json({ "message": error.message })
-    // }
+    if (req.body.length < 3 || !(Object.hasOwn(req.body, 'name') && Object.hasOwn(req.body, 'email') && Object.hasOwn(req.body, 'phone'))) {
+        throw RequestError(400, "Missing required name field")
+    } else {
+        res.status(201).json(await addContactData(req.body))
+    }
 }
 
 module.exports = postOneContact
