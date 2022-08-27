713a77b7673c8d665475b0fd86dd827747ff12b7
Lilia Lomaka <llomaka80@gmail.com>
2022-08-27 20:46:05 +0300

    Adding contacts filtering on favorite field

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -3,13 +3,26 @@ const { contactsServices } = require('../../services')
 const getContacts = async (req, res) => {
     const page = parseInt(req.query.page) || 1
     const limit = parseInt(req.query.limit) || 20
+    const favorite = req.query.favorite || null
     const { _id } = req.user
-    const totalCount = await contactsServices.countContactsData({ owner: _id })
+    let totalCount
+    let results
+    if (favorite !== null) {
+        totalCount = await contactsServices.countContactsData({ owner: _id, favorite })    
+    } else {
+        totalCount = await contactsServices.countContactsData({ owner: _id })
+    }
     const startIndex = (page - 1) * limit
     const totalPages = Math.ceil(totalCount / limit)
+    if (favorite !== null) {
+        results = await contactsServices.listContactsData({ owner: _id, favorite }, startIndex, limit)
+    } else {
+        results = await contactsServices.listContactsData({ owner: _id }, startIndex, limit)
+    }
 
     res.json({
-        results: await contactsServices.listContactsData({ owner: _id }, startIndex, limit),
+        results,
+        totalCount,
         page,
         pages: totalPages
     })
