4fdc0eebc4fa83bdd825c079e2964ea3024598cf
Lilia Lomaka <llomaka80@gmail.com>
2022-08-28 11:32:54 +0300

    Adding token regexp verification

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,13 @@ const { RequestError } = require('../helpers')
 const { usersServices } = require('../services')
 const jwt = require('jsonwebtoken')
 const { SECRET_KEY } = process.env
+const tokenRegex = /^([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_\-\+\/=]*)/
 
 const auth = async (req, _, next) => {
     const { authorization = '' } = req.headers
     const [bearer, token] = authorization.split(' ')
     try {
-        if (bearer !== 'Bearer') {
+        if (bearer !== 'Bearer' || !tokenRegex.test(token)) {
             throw RequestError(401, 'Not authorized')
         }
         const { id } = jwt.verify(token, SECRET_KEY)
@@ -18,7 +19,7 @@ const auth = async (req, _, next) => {
         req.user = user
         next()
     } catch (error) {
-        if (error.message === 'Invalid signature' || error.message === 'jwt expired') {
+        if (error.message.toLowerCase() === 'invalid signature' || error.message.toLowerCase() === 'jwt expired' || error.message.toLowerCase() === 'jwt malformed' || error.message.toLowerCase() ===  'invalid token') {
             error.status = 401
         }
         next(error)
