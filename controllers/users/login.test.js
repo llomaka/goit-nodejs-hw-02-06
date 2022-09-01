61f59250dde35dcf515042050198aadd25e71b3f
Lilia Lomaka <llomaka80@gmail.com>
2022-09-01 14:56:10 +0300

    Jest tests completed

--- controllers/users/login.test.js
+++ controllers/users/login.test.js
@@ -1,20 +1,51 @@
-const express = require('express')
 const request = require('supertest')
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+const app = require('../../app')
+const { DB_HOST, PORT = 3000 } = process.env
 const login = require('./login')
-const controllerWrapper = require('../../helpers')
-
-const app = express()
+const testUser = {"email": "llomaka80@gmail.com", "password": "M0ngoDB20"}
 
 describe('Login controller test', () => {
-    beforeAll(() => app.listen(3003))
-    afterAll(() => app.close())
-    test('Login returns response that includes 200 status code, token and object with email and subscription fields of String type', async () => {
-        const response = await request(app).post("/api/users/login", controllerWrapper(login)).send({ "email": "new@mail.com", "password": "pa$sw0rd" })
-        expect(response.status).toBe(200)
-        expect(response.body.user).objectContaining({
-            email: expect.toBe('string'),
-            subscription: expect.toBe('string'),
+    let connect, server
+    beforeAll(async () => {
+        await mongoose.connect(DB_HOST, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then(() => {
+            server = app.listen(PORT)
+        }).catch(()=>process.exit(1))
+    })
+    afterAll(() => {
+        server.close((err) => {
+            mongoose.connection.close()
         })
+    })
+    test('Login returns response that includes 200 status code', async () => {
+        const response = await request(app).post("/api/users/login", login).send(testUser).set('Accept', 'application/json')
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toBeDefined()
+        expect(response.body.user).toHaveProperty('email')
+        expect(response.body.user).toHaveProperty('subscription')
+    })
+    test('Login returns token of String type', async () => {
+        const response = await request(app).post("/api/users/login", login).send(testUser).set('Accept', 'application/json')
+        expect(response.body).toHaveProperty('token')
         expect(typeof response.body.token).toBe('string')
+    })
+    test('Login returns object with email and subscription fields of String type', async () => {
+        const response = await request(app).post("/api/users/login", login).send(testUser).set('Accept', 'application/json')
+        expect(typeof response.body.user.email).toBe('string')
+        expect(typeof response.body.user.subscription).toBe('string')
+    })
+    test('Login returns 400 when email or password is missing', async () => {
+        const response = await request(app).post("/api/users/login", login).send({ "password": "pa$sw0rd" }).set('Accept', 'application/json')
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toHaveProperty('message')
+    })
+    test('Login returns 401 when email or password is wrong', async () => {
+        const response = await request(app).post("/api/users/login", login).send({ "email": "llomaka80@gmail.com", "password": "pa$sw0rd" }).set('Accept', 'application/json')
+        expect(response.statusCode).toBe(401)
+        expect(response.body).toHaveProperty('message')
     })
 })
